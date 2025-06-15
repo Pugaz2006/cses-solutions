0136ab065b98399d243f7df1ebccb60e34f89f65
Pugaz2006 <pugaz2006@gmail.com>
2025-06-15 22:07:47 +0530

    some changes

---
+++ DP/Book_shop.cpp
@@ -0,0 +1,83 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define pb push_back
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define endl "\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve(){
+    int n=1,total=0;
+    string s;
+    cin>>n>>total;
+    vi wt(n);
+    cin>>wt;
+    vi val(n);
+    cin>>val;
+   // vector<vector<int>>dp(n,vector<int>(total+1,0));
+    //cout<<func(n-1,wt,val,total,dp);
+    vector<int>prev(total+1,0);
+    for(int i=1;i<=total;i++){
+        if(wt[0] <= i)prev[i] = val[0];
+    }
+    for (int i = 1; i < n; i++) {
+    vector<int> cur(total + 1, 0);
+    for (int tot = 1; tot <= total; tot++) {
+        int take = 0;
+        if (wt[i] <= tot) {
+            take = prev[tot - wt[i]] + val[i];
+        }
+        int nottake = prev[tot];
+        cur[tot] = max(take, nottake);
+    }
+    prev = cur; // Update previous row
+}
+    cout<<prev[total]<<endl;
+     
+    
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
